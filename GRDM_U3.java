2985ba29a47f25eddda6db2568fd7228ea33864b
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-05-25 19:59:02 +0200

    Added binary mode
    ToDo: 5 or 10 threshold.

--- GRDM_U3.java
+++ GRDM_U3.java
@@ -23,32 +23,31 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 /**
-     Opens an image window and adds a panel below the image
+ * Opens an image window and adds a panel below the image
  */
-public class GRDM_U3 implements PlugIn {
+public class GRDM_U3_s0577683 implements PlugIn {
 
 	ImagePlus imp; // ImagePlus object
 	private int[] origPixels;
 	private int width;
 	private int height;
 
-	String[] items = {"Original", "Rot-Kanal", "Graustufen"};
-
+	String[] items = { "Original", "Rot-Kanal", "Graustufen", "Negativbild", "Binärbild" };
 
 	public static void main(String args[]) {
 
 		IJ.open("bear.jpg");
-		//IJ.open("Z:/Pictures/Beispielbilder/orchid.jpg");
+		// IJ.open("Z:/Pictures/Beispielbilder/orchid.jpg");
 
-		GRDM_U3 pw = new GRDM_U3();
+		GRDM_U3_s0577683 pw = new GRDM_U3_s0577683();
 		pw.imp = IJ.getImage();
 		pw.run("");
 	}
 
 	public void run(String arg) {
-		if (imp==null) 
+		if (imp == null)
 			imp = WindowManager.getCurrentImage();
-		if (imp==null) {
+		if (imp == null) {
 			return;
 		}
 		CustomCanvas cc = new CustomCanvas(imp);
@@ -58,14 +57,12 @@ public class GRDM_U3 implements PlugIn {
 		new CustomWindow(imp, cc);
 	}
 
-
 	private void storePixelValues(ImageProcessor ip) {
 		width = ip.getWidth();
 		height = ip.getHeight();
 
-		origPixels = ((int []) ip.getPixels()).clone();
+		origPixels = ((int[]) ip.getPixels()).clone();
 	}
-
 
 	class CustomCanvas extends ImageCanvas {
 
@@ -75,18 +72,17 @@ public class GRDM_U3 implements PlugIn {
 
 	} // CustomCanvas inner class
 
-
 	class CustomWindow extends ImageWindow implements ItemListener {
 
 		private String method;
-		
+
 		CustomWindow(ImagePlus imp, ImageCanvas ic) {
 			super(imp, ic);
 			addPanel();
 		}
 
 		void addPanel() {
-			//JPanel panel = new JPanel();
+			// JPanel panel = new JPanel();
 			Panel panel = new Panel();
 
 			JComboBox cb = new JComboBox(items);
@@ -107,50 +103,45 @@ public class GRDM_U3 implements PlugIn {
 				method = item.toString();
 				changePixelValues(imp.getProcessor());
 				imp.updateAndDraw();
-			} 
-
+			}
 		}
-
 
 		private void changePixelValues(ImageProcessor ip) {
 
-			// Array zum Zurückschreiben der Pixelwerte
-			int[] pixels = (int[])ip.getPixels();
+			// Array zum ZurÃ¼ckschreiben der Pixelwerte
+			int[] pixels = (int[]) ip.getPixels();
 
 			if (method.equals("Original")) {
 
-				for (int y=0; y<height; y++) {
-					for (int x=0; x<width; x++) {
-						int pos = y*width + x;
-						
+				for (int y = 0; y < height; y++) {
+					for (int x = 0; x < width; x++) {
+						int pos = y * width + x;
+
 						pixels[pos] = origPixels[pos];
 					}
 				}
 			}
-			
+
 			if (method.equals("Rot-Kanal")) {
 
-				for (int y=0; y<height; y++) {
-					for (int x=0; x<width; x++) {
-						int pos = y*width + x;
-						int argb = origPixels[pos];  // Lesen der Originalwerte 
+				for (int y = 0; y < height; y++) {
+					for (int x = 0; x < width; x++) {
+						int pos = y * width + x;
+						int argb = origPixels[pos]; // Lesen der Originalwerte
 
 						int r = (argb >> 16) & 0xff;
-						//int g = (argb >>  8) & 0xff;
-						//int b =  argb        & 0xff;
+						// int g = (argb >> 8) & 0xff;
+						// int b = argb & 0xff;
 
-						int rn = r;
-						int gn = 0;
-						int bn = 0;
+						// Hier muessen die neuen RGB-Werte wieder auf den Bereich von 0 bis 255
+						// begrenzt werden
 
-						// Hier muessen die neuen RGB-Werte wieder auf den Bereich von 0 bis 255 begrenzt werden
-
-						pixels[pos] = (0xFF<<24) | (rn<<16) | (gn<<8) | bn;
+						pixels[pos] = (0xFF << 24) | (r << 16) | (0 << 8) | 0;
 					}
 				}
 			}
-			
-			// methode um Graustufen zu machen.
+
+			// Methode um Negativ zu machen.
 			if (method.equals("Negativbild")) {
 
 				for (int y = 0; y < height; y++) {
@@ -173,8 +164,8 @@ public class GRDM_U3 implements PlugIn {
 					}
 				}
 			}
-			
-			// methode um Graustufen zu machen.
+
+			// Methode um Graustufen zu machen.
 			if (method.equals("Graustufen")) {
 
 				for (int y = 0; y < height; y++) {
@@ -196,10 +187,33 @@ public class GRDM_U3 implements PlugIn {
 				}
 			}
 			
-			//Next Method Here	
-			
+			// Methode um Binär zu machen.
+			if (method.equals("Binärbild")) {
+
+				for (int y = 0; y < height; y++) {
+					for (int x = 0; x < width; x++) {
+						int pos = y * width + x;
+						int argb = origPixels[pos]; // Lesen der Originalwerte
+
+						int r = (argb >> 16) & 0xff;
+						int g = (argb >> 8) & 0xff;
+						int b = argb & 0xff;
+
+						int gray = (r + g + b) / 3;
+						
+						// Hier muessen die neuen RGB-Werte wieder auf den Bereich von 0 bis 255
+						// begrenzt werden
+						
+						if (gray>128) {
+							pixels[pos] = (0xFF << 24) | (255 << 16) | (255 << 8) | 255;
+						} else {
+							pixels[pos] = (0xFF << 24) | (0 << 16) | (0 << 8) | 0;
+						}
+					}
+				}
+			}
+
 		}
 
-
 	} // CustomWindow inner class
-} 
+}
